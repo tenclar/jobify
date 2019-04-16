bf98aa5d6b13fedf28690b1682eae4502b26c3c0
Tenclar Valus <tenclarvalus@gmail.com>
2019-04-16 00:33:09 -0500

    Initial version

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const app  = express()
+const bodyParser = require('body-parser')
+const sqlite = require('sqlite')
+const dbConnection =   sqlite.open('banco.sqlite',{Promise})
+
+ 
+app.set('view engine', 'ejs' )
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/', async(request, response) =>{
+    const db = await dbConnection
+    const categoriasDb = await db.all('select * from categorias')
+    
+    const vagas = await db.all('select * from vagas')
+
+    const categorias = categoriasDb.map(cat => {
+      return{
+        ...cat,
+        vagas: vagas.filter(vaga => vaga.categoria === cat.id)   
+      }
+    })
+
+    response.render('home', {
+       categorias
+   })
+})
+
+app.get('/vaga/:id', async(request, response) =>{
+    const db  = await dbConnection 
+    const vaga = await db.get('select * from vagas where id = ' + request.params.id )
+  
+    response.render('vaga', {
+      vaga
+    })
+ })
+
+ const init =  async() =>{
+    const db = await dbConnection  
+    await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY,  categoria TEXT); ')
+    await db.run('create table if not exists vagas (id INTEGER PRIMARY KEY,  categoria INTEGER, titulo TEXT, descricao TEXT); ')
+     // const vaga = 'Fullstack Developer (remote)'
+
+      //const descricao = 'Trabalhe como um desenvolvedor full stack na construção de um sistema CRM, resolvendo problemas junto a uma esteira ágil. Análise as necessidades e o ambiente para garantir que a solução que está sendo desenvolvida. Considere a arquitetura e o ambiente operacional atual e as futuras funcionalidades e aprimoramentos. Vivência com desenvolvimento em ambientes e times ágeis, aplicações de alta criticidade e implementação de APIs RESTful. Experiência em NodeJs e Angular 6, HTML, CSS, JavaScript, TypeScript, integração APIs REST, ORM e Git. Desejáveis conhecimentos em Linux e Docker.'
+     // await db.run(`insert into vagas(categoria, titulo, descricao) values(1,'${vaga}','${descricao}');`)
+    
+    //  await db.run(' delete  from vagas where vagas.id = 3' )
+    //  await db.run(' UPDATE vagas set vagas.categoria = 2 where vagas.id = 3 ' )
+  
+    // const categoria = 'Engineering team'
+    // const categoria = 'Marketing team'
+    // await db.run( ` insert into categorias(categoria) values('${categoria}');`)
+ }
+ init()
+
+
+ app.get('/admin', (req, res) =>{
+  res.render('admin/home')
+})
+
+
+app.get('/admin/vagas', async(req, res) =>{
+  const db = await dbConnection
+  const vagas = await db.all('select * from vagas')
+  res.render('admin/vagas', {
+    vagas
+  })
+})
+
+app.get('/admin/vaga/:id', async(req, res) =>{
+  const db  = await dbConnection 
+  const vaga = await db.get('select * from vagas where id = ' + req.params.id )
+  const categorias = await db.all('select * from categorias')
+  res.render('admin/vaga-editar', {
+    vaga, categorias
+  })
+})
+app.get('/admin/vaga', async(req, res) =>{
+  const db = await dbConnection
+  const categorias = await db.all('select * from categorias')
+ 
+    res.render('admin/vaga',{
+      categorias 
+    })
+})
+
+app.post('/admin/vaga', async(req,res)  => {
+     const {categoria,titulo, descricao } = req.body
+     const db  = await dbConnection 
+     await db.run(`insert into vagas(categoria, titulo, descricao) values(${categoria},'${titulo}','${descricao}');`)
+    
+  res.redirect('/admin/vagas')
+})
+
+app.listen(3000, (err) => {
+    if(err) {
+        console.log('Não foi possível iniciar o servidor do Jobify. ' )
+    } else {
+        console.log("Servidor do Jobify rodando...")
+    }
+})
