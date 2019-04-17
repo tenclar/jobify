c8e61a537419fc2d2943c09d0002088f53cbb57b
Tenclar Valus <tenclarvalus@gmail.com>
2019-04-17 15:27:57 -0500

    Aterado porta de acesso

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app  = express()
 const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection =   sqlite.open('banco.sqlite',{Promise})
-
+const port = process.env.PORT || 3000
  
 app.set('view engine', 'ejs' )
 app.use(express.static('public'))
