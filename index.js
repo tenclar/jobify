63b8b35aa28ddf5a26db20d9b9fdb184408d4ce4
Tenclar Valus <tenclarvalus@gmail.com>
2019-04-17 15:50:07 -0500

    configuração de acesso ao banco de dados

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app  = express()
 const bodyParser = require('body-parser')
 
-const path = require("path")
+const path = require('path')
 
 const sqlite = require('sqlite')
 const dbConnection =   sqlite.open(path.resolve(__dirname,'banco.sqlite'),{Promise})
