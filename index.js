6b9dff1ab7dac30b3f32c178abdd0de8c630d9ae
Tenclar Valus <tenclarvalus@gmail.com>
2019-04-17 15:57:04 -0500

    adicionado path para views

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const sqlite = require('sqlite')
 const dbConnection =   sqlite.open(path.resolve(__dirname,'banco.sqlite'),{Promise})
 const port = process.env.PORT || 3000
  
+app.set('views', path.join(_dirname, 'views'))
 app.set('view engine', 'ejs' )
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
